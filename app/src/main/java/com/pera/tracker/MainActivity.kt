8c1d22f6964310158c4f9924c4940e8a4ade6e5d
imjustyrex-wq <imjustyrex@gmail.com>
2026-09-06 01:51:56 +0800

    First build

---
+++ app/src/main/java/com/pera/tracker/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.pera.tracker
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+                    val tv = TextView(this)
+                            tv.text = "Hello Pera Tracker"
+                                    tv.textSize = 24f
+                                            setContentView(tv)
+                                                }
+                                                }
