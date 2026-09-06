e6a50fe83a2e23ff90ba953731bd3a31c8b78738
imjustyrex-wq <imjustyrex@gmail.com>
2026-09-06 21:08:04 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,17 @@
 pluginManagement {
-        repositories {
-                    google()
-                            mavenCentral()
-                                    gradlePluginPortal()
-        }
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
 dependencyResolutionManagement {
-        repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-            repositories {
-                        google()
-                                mavenCentral()
-            }
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
 }
-rootProject.name = "SinTrack"
+rootProject.name = "PeraTracker"
 include(":app")
